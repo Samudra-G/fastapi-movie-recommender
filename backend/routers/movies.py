ffc26e2de1dcfabd7ba51aa9d06330df1249e9e5
Samudra <samudramukhar@gmail.com>
2025-03-06 22:04:30 +0530

    added slowapi rate limiter

--- backend/routers/movies.py
+++ backend/routers/movies.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List, Optional
 from backend.database.schemas import MovieCreate, MovieResponse
@@ -7,6 +7,10 @@ from backend.database.database import get_db
 from backend.database.schemas import TokenData
 from backend.auth.admin import admin_required
 from backend.services.movie_services import MovieService
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+
+limiter = Limiter(key_func=get_remote_address)
 
 router = APIRouter(
     prefix = "/movies",
@@ -15,18 +19,21 @@ router = APIRouter(
 
 #add movie
 @router.post("/", response_model=MovieResponse, status_code=201)
-async def create_movie(movie: MovieCreate, db: AsyncSession = Depends(get_db),
+@limiter.limit("5/minute")
+async def create_movie(request: Request, movie: MovieCreate, db: AsyncSession = Depends(get_db),
                   current_user: TokenData = Depends(admin_required)):
     return await MovieService.create_movie(movie, db)
     
 #get movie by id
 @router.get("/{movie_id}", response_model=MovieResponse, status_code=200)
-async def get_movie(movie_id: int, db : AsyncSession = Depends(get_db)):
+@limiter.limit("50/minute")
+async def get_movie(request: Request, movie_id: int, db : AsyncSession = Depends(get_db)):
     return await MovieService.get_movie(movie_id, db)
 
 #get all movies or by genre
 @router.get("/", response_model=List[MovieResponse])
-async def get_movies(genre: Optional[str] = None, db: AsyncSession = Depends(get_db)):    
+@limiter.limit("50/minute")
+async def get_movies(request: Request, genre: Optional[str] = None, db: AsyncSession = Depends(get_db)):    
     return await MovieService.get_movies(genre, db)
 
 #update movie: will add to admin privilege later
